bfb203ba5881539be080fcef54e1aa403250b93a
patelashok <ashok.patel2917@gmail.com>
2021-06-24 11:35:29 +0530

    refactor CustomException2

---
+++ main/java/com/opencsv/CensusAnalyserException.java
@@ -0,0 +1,24 @@
+package com.opencsv;
+
+/**
+ * this is a child class of Exception which will print the exception message.
+ */
+public class CensusAnalyserException extends Exception {
+    enum ExceptionType {
+        CENSUS_FILE_PROBLEM
+    }
+    ExceptionType type;
+    /**
+     * here i have created custom exception.
+     * @param message
+     * @param type
+     */
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
---
+++ main/java/com/opencsv/IndiaCensusCSV.java
@@ -0,0 +1,34 @@
+package com.opencsv;
+
+
+import com.opencsv.bean.CsvBindByName;
+
+/**
+ * here in this class @CsvBindByName is an annotation which will take column value and boolean value as a parameter.
+ */
+public class IndiaCensusCSV {
+    @CsvBindByName(column = "State", required = true)
+    public String state;
+
+    @CsvBindByName(column = "Population", required = true)
+    public int population;
+
+    @CsvBindByName(column = "AreaInSqKm", required = true)
+    public int areaInSqKm;
+
+    @CsvBindByName(column = "DensityPerSqKm", required = true)
+    public int densityPerSqKm;
+
+    /**
+     * @return all values by printing all.
+     */
+    @Override
+    public String toString() {
+        return "IndiaCensusCSV{" +
+                "State='" + state + '\'' +
+                ", Population='" + population + '\'' +
+                ", AreaInSqKm='" + areaInSqKm + '\'' +
+                ", DensityPerSqKm='" + densityPerSqKm + '\'' +
+                '}';
+    }
+}
